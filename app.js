4a32398dbaf35beda30cd886ea69e1e26ce10ba3
Shwetha-703 <shweta.shingare11@gmail.com>
2024-01-17 15:48:17 +0530

    Igniting the app

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 
-{/* <div id="parent">
-    <div id="child1">
-        <h1>Im child 1 h1 tag</h1>
-        <h2>Im child 1 h2 tag</h2>
-    </div>
-
-    <div id="child2">
-        <h1>Im child 2 h1 tag</h1>
-        <h2>Im child 2 h2 tag</h2>
-    </div>
-</div>
- */}
+import React from "react"
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id:"parent"}, [
     React.createElement("div", {id:"child1"}, [React.createElement("h1",{},"Im child 1 h1 tag"),React.createElement("h1",{},"Im child 1 h1 tag")]),
